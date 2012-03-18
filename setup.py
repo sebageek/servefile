152c5275aa64c1b7a0c1e842e1a05d57baf70053
Sebastian Lohff <seba@someserver.de>
2012-03-18 03:58:18 +0100

    Removed README stub, now v0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	description='Script to serve files via a small HTTP server',
 	long_description='Script to serve files via a small HTTP server. The server redirects all http requests to the file, so only IP and port must be given to another user to access the file.',
 	platforms='posix',
-	version='0.3',
+	version='0.3.1',
 	license='GPLv3 or later',
 	url='http://seba-geek.de/stuff/servefile/',
 	author='Sebastian Lohff',
