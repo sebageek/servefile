32fb52e4d6f2e5d1c2dd1fd60788994bb66d790e
Sebastian Lohff <seba@someserver.de>
2012-04-16 19:12:58 +0200

    Moved to version 0.4.0

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup
 setup(
 	name='servefile',
 	description='Serve files from shell via a small HTTP server',
-	long_description='Serve files from shell via a small HTTP server. The server redirects all HTTP requests to the file, so only IP and port must be given to another user to access the file. Its main purpose is to quickly send a file to users in your local network, independent of their current setup (OS/software).',
+	long_description='Serve files from shell via a small HTTP server. The server redirects all HTTP requests to the file, so only IP and port must be given to another user to access the file. Its main purpose is to quickly send a file to users in your local network, independent of their current setup (OS/software). Beneath that it also supports uploads, SSL, HTTP basic auth and directory listings.',
 	platforms='posix',
-	version='0.3.2',
+	version='0.4.0',
 	license='GPLv3 or later',
 	url='http://seba-geek.de/stuff/servefile/',
 	author='Sebastian Lohff',
