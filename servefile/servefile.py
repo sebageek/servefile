11a7d8bd13f0afd75ae3379cfbb58297d71d3061
Sebastian Lohff <seba@someserver.de>
2020-09-30 01:44:57 +0200

    Release v0.5.1

--- servefile/servefile.py
+++ servefile/servefile.py
@@ -7,7 +7,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 import argparse
 import base64
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     platforms='posix',
-    version='0.5.0',
+    version='0.5.1',
     license='GPLv3 or later',
     url='https://github.com/sebageek/servefile/',
     author='Sebastian Lohff',
