876d19070394d38e4b9f47737e3229b5094d5364
Sebastian Lohff <seba@someserver.de>
2023-01-23 20:50:13 +0100

    Ignore python3.11 cgi deprecation warning
    The cgi module is marked as deprecated and will be removed in
    python3.13. servefile uses the module for its FieldStorage class used in
    the upload functionality. For now I will just ignore this, so servefile
    doesn't print out the warning each time it is run, but soon this will
    require either a rewrite of FieldStorage or an external library.
    
    With this commit we also now officially support python3.10 and
    python3.11.

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Communications',
         'Topic :: Communications :: File Sharing',
         'Topic :: Internet',
